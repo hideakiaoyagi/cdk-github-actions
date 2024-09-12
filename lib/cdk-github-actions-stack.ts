2d3f4efb8a4d3370287fc771252d7b86629ba579
hideakiaoyagi <20303346+hideakiaoyagi@users.noreply.github.com>
2024-09-12 10:55:20 +0900

    first commit

--- lib/cdk-github-actions-stack.ts
+++ lib/cdk-github-actions-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+// import lambda = require('@aws-cdk/aws-lambda');
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+// import apigw = require('@aws-cdk/aws-apigateway');
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +16,20 @@ export class CdkGithubActionsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkGithubActionsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      // runtime: lambda.Runtime.NODEJS_10_X,      // execution environment
+      runtime: lambda.Runtime.NODEJS_18_X,      // execution environment
+      // code: lambda.Code.asset('src/'),  // code loaded from the "lambda" directory
+      code: lambda.Code.fromAsset('src/'), // code loaded from "lambda" directory
+      handler: 'lambda/hello.handler'                // file is "hello", function is "handler"
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+      endpointTypes: [ apigw.EndpointType.REGIONAL ],
+    });
   }
 }
