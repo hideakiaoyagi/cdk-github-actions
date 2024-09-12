9604e5b7be0057e5129ee255ef06baa01ba741ad
hideakiaoyagi <20303346+hideakiaoyagi@users.noreply.github.com>
2024-09-12 20:44:36 +0900

    test

--- src/lambda/hello.ts
+++ src/lambda/hello.ts
@@ -1,9 +1,10 @@
 import { message } from "../model/message";
 
 export const handler = async (event: any = {}): Promise<any> => {
+    const env = process.env.toString();
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: message(event.path)
+        body: message(event.path, env)
     };
 }
--- src/model/message.ts
+++ src/model/message.ts
@@ -1,5 +1,4 @@
-export function message(path: string) {
-    const env = process.env
+export function message(path: string, test: string) {
     // return `Hello, CDK! You've hit ${path}\n`
-    return `Hello, ${env.TEST}! You've hit ${path}\n`
+    return `Hello, ${test}! You've hit ${path}\n`
 }
